2745c230ef15695c49f8df6592d17abe7fc66fe3
Caio Sobreiro <caiosobreiro>
2025-08-03 23:46:00 +0200

    Expose basic conversion API as a lib

--- gdcmconv.go
+++ gdcmconv.go
@@ -1,6 +1,9 @@
 package gdcmconv
 
-func Convert() error {
-	// Conversion logic goes here
-	return nil
+import (
+	"github.com/caio-sobreiro/gdcmconv-go/internal/gdcm"
+)
+
+func Convert(inputFile string, outputFile string) error {
+	return gdcm.ConvertToJPEG2000(inputFile, outputFile)
 }
